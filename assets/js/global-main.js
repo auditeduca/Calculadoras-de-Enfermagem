1e744c31d36ea52262bbe5aabfa8445b9db08d88
Leivis <leivis.lima@aol.com>
2025-12-19 01:46:10 -0300

    missao

---
+++ assets/js/global-main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Scroll suave para links internos
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            if (targetId === '#') return;
+
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 20,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
